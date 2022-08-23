20828361d6746829a11b3c05ea0740ec9a93d2da
Kieran <kieranrobson1999@gmail.com>
2022-08-23 20:17:32 +0100

    Changed status

--- events/ready.js
+++ events/ready.js
@@ -2,9 +2,5 @@ const client = require("../index");
 
 client.on('ready', () => {
     console.log('Bot is online')
-
-    setInterval(() =>{
-    client.user.setActivity(`!help`, {type:"WATCHING"})
-    }, 180000);
-
+    client.user.setActivity(`The High Seas`, {type:"WATCHING"})
 })
